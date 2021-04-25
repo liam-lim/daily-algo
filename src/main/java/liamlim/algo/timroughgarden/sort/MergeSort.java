44cb71bd659538edb17765e0c544f7bd03f8d4e7
Liam Lim <liamlim.soe@gmail.com>
2021-04-25 21:32:33 +0800

    Implemented merge sort

---
+++ src/main/java/liamlim/algo/timroughgarden/sort/MergeSort.java
@@ -0,0 +1,68 @@
+package liamlim.algo.timroughgarden.sort;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class MergeSort {
+
+  @Test
+  public void test_mergeSort() {
+    List<Integer> input = Arrays.asList(5, 4, 1, 8, 7, 2, 6, 3, 9);
+    List<Integer> output = mergeSort(input);
+
+    Collections.sort(input);
+    System.out.println("actual " + output);
+    System.out.println("expected " + input);
+
+    Assert.assertEquals(output, input);
+  }
+
+  private static List<Integer> mergeSort(List<Integer> input) {
+
+    if (input.size() < 2) {
+      return input;
+    }
+
+    List<Integer> jl = input.subList(0, input.size() / 2);
+    List<Integer> kl = input.subList(input.size() / 2, input.size());
+
+    return merge(mergeSort(jl), mergeSort(kl));
+  }
+
+  private static List<Integer> merge(List<Integer> jl, List<Integer> kl) {
+
+    ArrayList<Integer> output = new ArrayList<>();
+
+    int j = 0;
+    int k = 0;
+
+    while (j != jl.size() || k != kl.size()) {
+
+      if (j == jl.size()) {
+        output.addAll(kl.subList(k, kl.size()));
+        break;
+      }
+
+      if (k == kl.size()) {
+        output.addAll(jl.subList(j, jl.size()));
+        break;
+      }
+
+      if (jl.get(j) < kl.get(k)) {
+        output.add(jl.get(j));
+        j++;
+      }
+      else {
+        output.add(kl.get(k));
+        k++;
+      }
+    }
+
+    return output;
+  }
+}
